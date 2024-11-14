ace517366704ca4d1c9abbd720ddd4da85c1aa9a
Venkat-Ratna <ratnachowdary56@gmail.com>
2024-11-14 19:17:14 +0530

    frontend init

---
+++ Frontend/src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import {} from '$lib/index.css';
+
+	interface Props {
+		children?: import('svelte').Snippet;
+	}
+	let { children }: Props = $props();
+</script>
+
+<div>
+	<header>This is a header</header>
+	<main>
+		{@render children?.()}
+	</main>
+	<footer>This is a footer</footer>
+</div>
+
+<style>
+	div {
+		max-width: 108rem;
+		margin-inline: auto;
+		padding-inline: 0.85em;
+		/* border: 2px solid lime; */
+	}
+</style>
